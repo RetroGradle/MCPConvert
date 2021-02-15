f7e624e90ec4cff28fe85a79e1cf8922efd4bc9f
sciwhiz12 <arnoldnunag12@gmail.com>
2021-02-15 21:55:43 +0800

    Filter out PK declarations from the SRG before TSRG conversion

--- src/main/java/uk/gemwire/mcpconvert/convert/JoinedSrgConverter.java
+++ src/main/java/uk/gemwire/mcpconvert/convert/JoinedSrgConverter.java
@@ -2,7 +2,9 @@ package uk.gemwire.mcpconvert.convert;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Collectors;
 
 import net.minecraftforge.srgutils.IMappingFile;
 
@@ -10,7 +12,16 @@ import net.minecraftforge.srgutils.IMappingFile;
  * @author Sm0keySa1m0n
  */
 public class JoinedSrgConverter {
-    public static void convert(InputStream srgInput, Path tsrgOutput) throws IOException {
-        IMappingFile.load(srgInput).write(tsrgOutput, IMappingFile.Format.TSRG, false);
+    public static void convert(Path srgInput, Path tempFile, Path tsrgOutput) throws IOException {
+        // Filter out the package declarations ('PK: ') from the SRG
+        Files.write(tempFile,
+            Files.lines(srgInput)
+                .filter(str -> !str.startsWith("PK:"))
+                .collect(Collectors.toUnmodifiableList())
+        );
+
+        try (InputStream input = Files.newInputStream(tempFile)) {
+            IMappingFile.load(input).write(tsrgOutput, IMappingFile.Format.TSRG, false);
+        }
     }
 }
