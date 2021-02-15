dcc744e354c9663d2bc61e24ed88b2cff44244cb
sciwhiz12 <arnoldnunag12@gmail.com>
2021-02-15 17:47:12 +0800

    Refactor JoinedExcSplitter and add tests (#12)

--- src/main/java/uk/gemwire/mcpconvert/convert/JoinedExcSplitter.java
+++ src/main/java/uk/gemwire/mcpconvert/convert/JoinedExcSplitter.java
@@ -7,19 +7,24 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.compile;
 
 /**
  * @author Sm0keySa1m0n
  */
 public class JoinedExcSplitter {
 
-    private static final Pattern CONSTRUCTOR_REGEX = Pattern.compile("(\\.<init>)(\\(\\S*)(=\\S*\\|p_i)(\\d*)");
-    private static final Pattern EXCEPTION_REGEX = Pattern.compile("(\\(\\S*\\)\\S*)=(\\S+)\\|");
-    private static final Pattern ACCESS_REGEX = Pattern.compile("(\\S*)\\.(\\S*)(\\(\\S*\\)\\S*)-Access=(\\S*)");
+    private static final Pattern CONSTRUCTOR_REGEX =
+        compile("(?<class>\\S*?)(?:\\.<init>)(?<desc>\\S*)(?:=\\S*\\|p_i)(?<id>\\d*)");
+    private static final Pattern EXCEPTION_REGEX =
+        compile("(?<class>\\S*)\\.(?<func>\\S*)(?<desc>\\(\\S*\\)\\S*)=(?<exceptions>\\S+)\\|");
+    private static final Pattern ACCESS_REGEX =
+        compile("(?<class>\\S*)\\.(?<func>\\S*)(?<desc>\\(\\S*\\)[^=\\s]).*-Access=(?<access>\\S*)");
 
-    @SuppressWarnings("StringBufferReplaceableByString")
     public static Result parseExc(Path excFile) throws IOException {
         List<String> lines = Files.readAllLines(excFile, StandardCharsets.UTF_8);
 
@@ -27,78 +32,87 @@ public class JoinedExcSplitter {
         List<String> exceptionLines = new ArrayList<>();
         List<String> accessLines = new ArrayList<>();
 
-        for (String line : lines) {
-            boolean lineMatched = false;
-            // If we match ".<init>" we're a constructor.
-            Matcher constructorMatcher = CONSTRUCTOR_REGEX.matcher(line);
-            // If we match (SOMETHING)SOMETHING=SOMETHING|, we're an exception line
-            Matcher exceptionMatcher = EXCEPTION_REGEX.matcher(line);
-            Matcher accessMatcher = ACCESS_REGEX.matcher(line);
-
-            if (constructorMatcher.find()) {
-                // constructors.txt lines are in the form
-                // SRG CLASS SIGNATURE
-
-                // Matches are in the order
-                // .<init> SIGNATURE =|p_i SRG
-                // CLASS is line[0]..match0
-
-                final String className = line.substring(0, constructorMatcher.start(0));
-
-                StringBuilder constructorBuilder = new StringBuilder()
-                    .append(constructorMatcher.group(4)).append(" ")
-                    .append(className).append(" ")
-                    .append(constructorMatcher.group(2));
-
-                constructorLines.add(constructorBuilder.toString());
-                lineMatched = true;
-            }
-
-            if (exceptionMatcher.find()) {
-                // exceptions.txt is in the form
-                // CLASS/FUNCTION (PARAMS)RETURN EXCEPTION
-
-                // Matches are in the order
-                // (PARAMS)RETURN = EXCEPTION |
-
-                final String[] combinedClassMethod = line.substring(0, exceptionMatcher.start(0)).split("\\.");
-                final String className = combinedClassMethod[0];
-                final String methodName = combinedClassMethod[1];
-
-                StringBuilder exceptionBuilder = new StringBuilder()
-                    .append(className).append(" ")
-                    .append(exceptionMatcher.group(1)).append(" ");
-                exceptionBuilder.append(String.join(" ", exceptionMatcher.group(2).split(",")));
-                exceptionLines.add(exceptionBuilder.toString());
-                lineMatched = true;
-            }
-
-            if (accessMatcher.find()) {
-                // access.txt is in the form
-                // ACCESS CLASS OBJECT SIGNATURE
-
-                // accessMatches is in the form
-                // CLASS OBJECT SIGNATURE ACCESS
-                StringBuilder accessBuilder = new StringBuilder()
-                    .append(accessMatcher.group(4)).append(" ")
-                    .append(accessMatcher.group(1)).append(" ")
-                    .append(accessMatcher.group(2)).append(" ")
-                    .append(accessMatcher.group(3));
-
-                accessLines.add(accessBuilder.toString());
-                lineMatched = true;
-            }
-
-            if (!lineMatched) {
-                System.err.println("JoinedExcSplitter: No useful data on line: " + line);
-            }
-        }
+        lines.forEach(line -> parseExcLine(line, constructorLines::add, exceptionLines::add, accessLines::add));
 
         Collections.sort(constructorLines);
         Collections.sort(exceptionLines);
         Collections.sort(accessLines);
 
         return new Result(List.copyOf(constructorLines), List.copyOf(exceptionLines), List.copyOf(accessLines));
+    }
+
+    public static void parseExcLine(String line,
+        Consumer<String> constructorCallback,
+        Consumer<String> exceptionCallback,
+        Consumer<String> accessCallback) {
+        parseExcLine(line, constructorCallback, exceptionCallback, accessCallback, false);
+    }
+
+    public static void parseExcLine(String line,
+        Consumer<String> constructorCallback,
+        Consumer<String> exceptionCallback,
+        Consumer<String> accessCallback,
+        boolean silent) {
+        boolean lineMatched = false;
+        // If we match ".<init>" we're a constructor.
+        Matcher constructorMatcher = CONSTRUCTOR_REGEX.matcher(line);
+        // If we match (SOMETHING)SOMETHING=SOMETHING|, we're an exception line
+        Matcher exceptionMatcher = EXCEPTION_REGEX.matcher(line);
+        Matcher accessMatcher = ACCESS_REGEX.matcher(line);
+
+        if (constructorMatcher.find()) {
+            // constructors.txt lines are in the form
+            // SRG CLASS SIGNATURE
+
+            // Matches are in the order
+            // .<init> SIGNATURE =|p_i SRG
+            // CLASS is line[0]..match0
+            constructorCallback.accept(
+                String.join(" ",
+                    constructorMatcher.group("id"),
+                    constructorMatcher.group("class"),
+                    constructorMatcher.group("desc"))
+            );
+            lineMatched = true;
+        }
+
+        if (exceptionMatcher.find()) {
+            // exceptions.txt is in the form
+            // CLASS/FUNCTION (PARAMS)RETURN EXCEPTION
+
+            // Matches are in the order
+            // (PARAMS)RETURN = EXCEPTION |
+            exceptionCallback.accept(
+                String.join(" ",
+                    exceptionMatcher.group("class"),
+                    exceptionMatcher.group("func"),
+                    exceptionMatcher.group("desc"),
+                    String.join(" ",
+                        exceptionMatcher.group("exceptions").split(","))
+                )
+            );
+            lineMatched = true;
+        }
+
+        if (accessMatcher.find()) {
+            // access.txt is in the form
+            // ACCESS CLASS OBJECT SIGNATURE
+
+            // accessMatches is in the form
+            // CLASS OBJECT SIGNATURE ACCESS
+            accessCallback.accept(
+                String.join(" ",
+                    accessMatcher.group("access"),
+                    accessMatcher.group("class"),
+                    accessMatcher.group("func"),
+                    accessMatcher.group("desc"))
+            );
+            lineMatched = true;
+        }
+
+        if (!lineMatched && !silent) {
+            System.err.println("JoinedExcSplitter: No useful data on line: " + line);
+        }
     }
 
     public static record Result(List<String> constructors, List<String> exceptions, List<String> access) {
